a5afff4145a28cb789818c2779406318dc113f22
auliasalwafitri30-sketch <auliasalwafitri30>
2026-04-02 07:47:03 +0700

    first commit

---
+++ modul/menu.php
@@ -0,0 +1,16 @@
+<!-- Categories -->
+    <div class="bg-white border-b">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex space-x-8 overflow-x-auto py-4">
+                <?php
+                $sqlmenu = $conn->query("SELECT * FROM categories");
+                while($datamenu = $sqlmenu->fetch_assoc()){
+                ?> 
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium"><i class="<?= $datamenu['icon']?>"></i><?= $datamenu['name'] ?></a>
+                <?php
+                }
+                ?>
+                
+            </div>
+        </div>
+    </div>
